333b015fcb788a73e8a8c8ae78070afd050e3149
Jam <jamhejratipro@gmail.com>
2022-03-08 22:34:33 +0330

    comment added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -269,3 +269,5 @@ attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 healBtn.addEventListener("click", healPlayerHandler);
 logBtn.addEventListener("click", printLogHandler);
+
+// test
